d9cbd3a8bcc8141232dfc40e10e62d12085f0526
Nathaly Diaz <nathalydiaz@mail.weber.edu>
2025-08-06 14:02:18 -0600

    atlas connection

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const config = require("./config.json");
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
